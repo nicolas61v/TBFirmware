f5206da9f547dca9a585ca6159b0ebcf78afdc4c
MejiaDeRul <165450391+MejiaDeRul@users.noreply.github.com>
2024-06-12 12:03:18 -0500

    Update test.ino

--- test.ino
+++ test.ino
@@ -1,245 +1,112 @@
-/* ======================================== Including the libraries. */
-#include "esp_camera.h"
-#include "soc/soc.h"
-#include "soc/rtc_cntl_reg.h"
-#include "quirc.h"
-#include "Arduino.h"
-#include "SD.h"    // Nuevo
-#include "Audio.h" // Nuevo
+/* 
+ * WAV Recorder for Seeed XIAO ESP32S3 Sense 
+ * 
+ * NOTE: To execute this example, we will need to use the PSRAM 
+ * function of the ESP-32 chip, so please turn it on before uploading.
+ * Tools>PSRAM: "OPI PSRAM"
+
+
+*/
+
+#include <I2S.h>
+#include "FS.h"
+#include "SD.h"
 #include "SPI.h"
 
-#define SD_CS  21 // GPIO0 (D8) --- Nuevo
+// make changes as needed
+#define RECORD_TIME   20  // seconds, The maximum value is 240
+#define WAV_FILE_NAME "arduino_rec"
 
-// Audio -- Nuevo
-#define MAX98357A_I2S_DOUT 4
-#define MAX98357A_I2S_BCLK 5
-#define MAX98357A_I2S_LRC  6
-Audio audio;
-/* ======================================== */
+// do not change for best
+#define SAMPLE_RATE 16000U
+#define SAMPLE_BITS 16
+#define WAV_HEADER_SIZE 44
+#define VOLUME_GAIN 2
 
-// creating a task handle
-TaskHandle_t QRCodeReader_Task; 
-
-/* ======================================== Select camera model */
-
-/* ======================================== */
-
-/* ======================================== GPIO of camera models */
-
-#define PWDN_GPIO_NUM     -1
-#define RESET_GPIO_NUM    -1
-#define XCLK_GPIO_NUM     10
-#define SIOD_GPIO_NUM     40
-#define SIOC_GPIO_NUM     39
-
-#define Y9_GPIO_NUM       48
-#define Y8_GPIO_NUM       11
-#define Y7_GPIO_NUM       12
-#define Y6_GPIO_NUM       14
-#define Y5_GPIO_NUM       16
-#define Y4_GPIO_NUM       18
-#define Y3_GPIO_NUM       17
-#define Y2_GPIO_NUM       15
-#define VSYNC_GPIO_NUM    38
-#define HREF_GPIO_NUM     47
-#define PCLK_GPIO_NUM     13
-
-/* ======================================== */
-
-/* ======================================== Variables declaration */
-struct QRCodeData
-{
-  bool valid;
-  int dataType;
-  uint8_t payload[1024];
-  int payloadLen;
-};
-
-struct quirc *q = NULL;
-uint8_t *image = NULL;  
-camera_fb_t * fb = NULL;
-struct quirc_code code;
-struct quirc_data data;
-quirc_decode_error_t err;
-struct QRCodeData qrCodeData;  
-String QRCodeResult = "";
-/* ======================================== */
-
-/* __________ VOID SETUP() */
 void setup() {
-  // Disable brownout detector.
-  WRITE_PERI_REG(RTC_CNTL_BROWN_OUT_REG, 0);
-
-  /* ---------------------------------------- Init serial communication speed (baud rate). */
   Serial.begin(115200);
-  Serial.setDebugOutput(true);
-  Serial.println();
-  /* ---------------------------------------- */
-
-  if (!SD.begin(SD_CS)) { // Nuevo
-    Serial.println("Error inicializando la tarjeta SD!");
-    return;
-  } else {
-    Serial.println("Tarjeta SD inicializada correctamente."); // Nuevo
+  while (!Serial) ;
+  
+  I2S.setAllPins(-1, 42, 41, -1, -1);
+  if (!I2S.begin(PDM_MONO_MODE, SAMPLE_RATE, SAMPLE_BITS)) {
+    Serial.println("Failed to initialize I2S!");
+    while (1) ;
   }
-
-  audio.setPinout(MAX98357A_I2S_BCLK, MAX98357A_I2S_LRC, MAX98357A_I2S_DOUT); // Nuevo
-  audio.setVolume(100);
-
-  /* ---------------------------------------- Camera configuration. */
-  Serial.println("Start configuring and initializing the camera...");
-  camera_config_t config;
-  config.ledc_channel = LEDC_CHANNEL_0;
-  config.ledc_timer = LEDC_TIMER_0;
-  config.pin_d0 = Y2_GPIO_NUM;
-  config.pin_d1 = Y3_GPIO_NUM;
-  config.pin_d2 = Y4_GPIO_NUM;
-  config.pin_d3 = Y5_GPIO_NUM;
-  config.pin_d4 = Y6_GPIO_NUM;
-  config.pin_d5 = Y7_GPIO_NUM;
-  config.pin_d6 = Y8_GPIO_NUM;
-  config.pin_d7 = Y9_GPIO_NUM;
-  config.pin_xclk = XCLK_GPIO_NUM;
-  config.pin_pclk = PCLK_GPIO_NUM;
-  config.pin_vsync = VSYNC_GPIO_NUM;
-  config.pin_href = HREF_GPIO_NUM;
-  config.pin_sscb_sda = SIOD_GPIO_NUM;
-  config.pin_sscb_scl = SIOC_GPIO_NUM;
-  config.pin_pwdn = PWDN_GPIO_NUM;
-  config.pin_reset = RESET_GPIO_NUM;
-  config.xclk_freq_hz = 10000000;
-  config.pixel_format = PIXFORMAT_GRAYSCALE;
-  config.frame_size = FRAMESIZE_QVGA;
-  config.jpeg_quality = 15;
-  config.fb_count = 1;
-  
-  #if defined(CAMERA_MODEL_ESP_EYE)
-    pinMode(13, INPUT_PULLUP);
-    pinMode(14, INPUT_PULLUP);
-  #endif
-
-  esp_err_t err = esp_camera_init(&config);
-  if (err != ESP_OK) {
-    Serial.printf("Camera init failed with error 0x%x", err);
-    ESP.restart();
+  if(!SD.begin(21)){
+    Serial.println("Failed to mount SD Card!");
+    while (1) ;
   }
-  
-  sensor_t * s = esp_camera_sensor_get();
-  s->set_framesize(s, FRAMESIZE_QVGA);
-  
-  Serial.println("Configure and initialize the camera successfully.");
-  Serial.println();
-  /* ---------------------------------------- */
-
-  /* ---------------------------------------- create "QRCodeReader_Task" using the xTaskCreatePinnedToCore() function */
-  xTaskCreatePinnedToCore(
-             QRCodeReader,          /* Task function. */
-             "QRCodeReader_Task",   /* name of task. */
-             10000,                 /* Stack size of task */
-             NULL,                  /* parameter of the task */
-             1,                     /* priority of the task */
-             &QRCodeReader_Task,    /* Task handle to keep track of created task */
-             0);                    /* pin task to core 0 */
-  Serial.println("QRCodeReader_Task created successfully."); // Nuevo
-  /* ---------------------------------------- */
+  record_wav();
 }
-/* __________ */
+
 void loop() {
-  // put your main code here, to run repeatedly:
-  delay(1);
+  delay(1000);
+  Serial.printf(".");
 }
-/* __________ */
 
-/* __________ The function to be executed by "QRCodeReader_Task" */
-// This function is to instruct the camera to take or capture a QR Code image, then it is processed and translated into text.
-void QRCodeReader( void * pvParameters ){
-  /* ---------------------------------------- */
-  Serial.println("QRCodeReader is ready.");
-  Serial.print("QRCodeReader running on core ");
-  Serial.println(xPortGetCoreID());
-  Serial.println();
-  /* ---------------------------------------- */
-
-  /* ---------------------------------------- Loop to read QR Code in real time. */
-  while(1){
-      q = quirc_new();
-      if (q == NULL){
-        Serial.print("can't create quirc object\r\n");  
-        continue;
-      }
-    
-      fb = esp_camera_fb_get();
-      if (!fb)
-      {
-        Serial.println("Camera capture failed");
-        continue;
-      }   
-      
-      Serial.println("Camera capture succeeded."); // Nuevo
-      
-      quirc_resize(q, fb->width, fb->height);
-      image = quirc_begin(q, NULL, NULL);
-      memcpy(image, fb->buf, fb->len);
-      quirc_end(q);
-      
-      int count = quirc_count(q);
-      if (count > 0) {
-        quirc_extract(q, 0, &code);
-        err = quirc_decode(&code, &data);
-    
-        if (err){
-          Serial.println("Decoding FAILED");
-          QRCodeResult = "Decoding FAILED";
-        } else {
-          Serial.printf("Decoding successful:\n");
-          dumpData(&data);
-        } 
-        Serial.println();
-      } else {
-        Serial.println("No QR code detected."); // Nuevo
-      }
-      
-      esp_camera_fb_return(fb);
-      fb = NULL;
-      image = NULL;  
-      quirc_destroy(q);
-  }
-  /* ---------------------------------------- */
-}
-/* __________ */
-
-/* __________ Function to display the results of reading the QR Code on the serial monitor. */
-void dumpData(const struct quirc_data *data)
+void record_wav()
 {
-  Serial.printf("Version: %d\n", data->version);
-  Serial.printf("ECC level: %c\n", "MLHQ"[data->ecc_level]);
-  Serial.printf("Mask: %d\n", data->mask);
-  Serial.printf("Length: %d\n", data->payload_len);
-  Serial.printf("Payload: %s\n", data->payload);
-  
-  QRCodeResult = (const char *)data->payload;
+  uint32_t sample_size = 0;
+  uint32_t record_size = (SAMPLE_RATE * SAMPLE_BITS / 8) * RECORD_TIME;
+  uint8_t *rec_buffer = NULL;
+  Serial.printf("Ready to start recording ...\n");
 
-  // Verificar si el texto del QR es "prender" o "apagar"
-  if (strcmp(QRCodeResult.c_str(), "encender") == 0) {
-    if (audio.connecttoFS(SD, "/Imagine.wav")) { //Nuevo
-      Serial.println("Reproduciendo Imagine.wav");
-    } else {
-      Serial.println("Error al reproducir Imagine.wav");
-    }
+  File file = SD.open("/"WAV_FILE_NAME".wav", FILE_WRITE);
+  // Write the header to the WAV file
+  uint8_t wav_header[WAV_HEADER_SIZE];
+  generate_wav_header(wav_header, record_size, SAMPLE_RATE);
+  file.write(wav_header, WAV_HEADER_SIZE);
 
-    while (audio.isRunning()) { // Nuevo
-      audio.loop();
-    }
-    Serial.println("Encendiendo el pin 12");
-  } else if (strcmp(QRCodeResult.c_str(), "apagar") == 0) {
-    if (audio.connecttoFS(SD, "/Imagine.wav")) { //Nuevo
-      Serial.println("Reproduciendo Imagine.wav");
-    } else {
-      Serial.println("Error al reproducir Imagine.wav");
-    }
-
-    while (audio.isRunning()) { // Nuevo
-      audio.loop();
-    }
+  // PSRAM malloc for recording
+  rec_buffer = (uint8_t *)ps_malloc(record_size);
+  if (rec_buffer == NULL) {
+    Serial.printf("malloc failed!\n");
+    while(1) ;
   }
+  Serial.printf("Buffer: %d bytes\n", ESP.getPsramSize() - ESP.getFreePsram());
+
+  // Start recording
+  esp_i2s::i2s_read(esp_i2s::I2S_NUM_0, rec_buffer, record_size, &sample_size, portMAX_DELAY);
+  if (sample_size == 0) {
+    Serial.printf("Record Failed!\n");
+  } else {
+    Serial.printf("Record %d bytes\n", sample_size);
+  }
+
+  // Increase volume
+  for (uint32_t i = 0; i < sample_size; i += SAMPLE_BITS/8) {
+    (*(uint16_t *)(rec_buffer+i)) <<= VOLUME_GAIN;
+  }
+
+  // Write data to the WAV file
+  Serial.printf("Writing to the file ...\n");
+  if (file.write(rec_buffer, record_size) != record_size)
+    Serial.printf("Write file Failed!\n");
+
+  free(rec_buffer);
+  file.close();
+  Serial.printf("The recording is over.\n");
+}
+
+void generate_wav_header(uint8_t *wav_header, uint32_t wav_size, uint32_t sample_rate)
+{
+  // See this for reference: http://soundfile.sapp.org/doc/WaveFormat/
+  uint32_t file_size = wav_size + WAV_HEADER_SIZE - 8;
+  uint32_t byte_rate = SAMPLE_RATE * SAMPLE_BITS / 8;
+  const uint8_t set_wav_header[] = {
+    'R', 'I', 'F', 'F', // ChunkID
+    file_size, file_size >> 8, file_size >> 16, file_size >> 24, // ChunkSize
+    'W', 'A', 'V', 'E', // Format
+    'f', 'm', 't', ' ', // Subchunk1ID
+    0x10, 0x00, 0x00, 0x00, // Subchunk1Size (16 for PCM)
+    0x01, 0x00, // AudioFormat (1 for PCM)
+    0x01, 0x00, // NumChannels (1 channel)
+    sample_rate, sample_rate >> 8, sample_rate >> 16, sample_rate >> 24, // SampleRate
+    byte_rate, byte_rate >> 8, byte_rate >> 16, byte_rate >> 24, // ByteRate
+    0x02, 0x00, // BlockAlign
+    0x10, 0x00, // BitsPerSample (16 bits)
+    'd', 'a', 't', 'a', // Subchunk2ID
+    wav_size, wav_size >> 8, wav_size >> 16, wav_size >> 24, // Subchunk2Size
+  };
+  memcpy(wav_header, set_wav_header, sizeof(set_wav_header));
 }
